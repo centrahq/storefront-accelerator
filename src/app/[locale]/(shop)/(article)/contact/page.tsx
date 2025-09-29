52752051de5fcdca6226b16bc36c4ca0e9d6d6bf
piotr-symonowicz <49191214+piotr-symonowicz@users.noreply.github.com>
2025-09-29 13:38:45 +0200

    Fix links

--- src/app/[locale]/(shop)/(article)/contact/page.tsx
+++ src/app/[locale]/(shop)/(article)/contact/page.tsx
@@ -15,11 +15,11 @@ export default function ContactPage() {
       <div className="mx-auto flex max-w-6xl flex-col gap-8">
         <h1 className="text-4xl font-medium">Contact Us</h1>
         <p>
-          If you're a partner looking for documentation, start on [centra.dev - our portal for developers](https://centra.dev)!
+          If you're a partner looking for documentation, start on <a href="https://centra.dev" target="_blank">centra.dev - our portal for developers</a>!
 
-          If you're new, why not [sign up for a demo](https://centra.com/demo)?
+          If you're new, why not <a href="https://centra.com/demo" target="_blank">sign up for a demo</a>?
 
-          If you're looking to contact Centra in general, please see [our contact page](https://centra.com/contact).
+          If you're looking to contact Centra in general, please see <a href="https://centra.com/contact" target="_blank">our contact page</a>.
         </p>
       </div>
     </div>
