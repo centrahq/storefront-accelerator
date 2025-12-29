93833e676a58e982bc8b9797cf09de68083302b1
rebzden-centra <kacper.brezden@centra.com>
2025-12-29 09:47:04 +0100

    chore: adjustments to the onAuthorized apple pay handling

--- src/features/checkout/components/Payment/types.ts
+++ src/features/checkout/components/Payment/types.ts
@@ -2,10 +2,10 @@ export interface AdyenAddress {
   address1: string;
   city: string;
   country: string;
-  email: string;
+  email?: string;
   firstName: string;
   lastName: string;
-  phoneNumber: string;
+  phoneNumber?: string;
   state: string;
   zipCode: string;
 }
